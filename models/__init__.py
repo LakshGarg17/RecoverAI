f4b2e0caa880b27369de6643c5858da550d3603d
Laksh Garg <lakshgarg98@gmail.com>
2026-08-26 08:13:13 +0530

    Delete models directory

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from backend.models.notice import NoticeStructuredData
-from backend.models.schemas import UploadResponse, ErrorResponse, HealthResponse
-
-__all__ = ["NoticeStructuredData", "UploadResponse", "ErrorResponse", "HealthResponse"]
